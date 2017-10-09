3903d32f54c73161ab59c57aba3aeb2dded0c40f
三宅祥太 <mish@miyakeshoutanoMacBook-Air.local>
2017-10-09 23:41:11 +0900

    ユーザー名

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,12 +11,16 @@ def new
 end
 
 def create
-  Tweet.create(tweet_params)
+  Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
 end
  
 private
 def tweet_params
   params.permit(:name, :image, :text)
+end
+
+def move_to_index
+  redirect_to action: :index unless user_signed_in?
 end
 
 
